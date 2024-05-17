0987a0097fbc4267671b9c62f517724e4cf93c94
brookinsm <brookinsm@gmail.com>
2024-05-16 19:28:00 -0500

    fixed typos for menu and submit menu

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
+
+
+
+
 //3.0 - moved to top added 5.0 to menu structure
 const menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-      {text: 'all', href: '/catalog/all'},
-      {text: 'top selling', href: '/catalog/top'},
-      {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-      {text: 'new', href: '/orders/new'},
-      {text: 'pending', href: '/orders/pending'},
-      {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-      {text: 'profile', href: '/account/profile'},
-      {text: 'sign out', href: '/account/signout'},
-    ]},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+
 
   //1.0
-  const MainEl = document.querySelector('main');
+  const mainEl = document.querySelector('main');
 //1.1
-MainEl.style.backgroundColor='var(--main-bg)';
+  mainEl.style.backgroundColor = 'var(--main-bg)';
+
 //1.2
-MainEl.innerHTML='<h1> SEI ROCKS!</h1>';
+mainEl.innerHTML='<h1> SEI ROCKS!</h1>';
 //1.3
-MainEl.classList.add('flex-ctr');
+mainEl.classList.add('flex-ctr');
 //2.0
-const topMenuEL=document.getElementById('top-menu');
+const topMenuEl = document.getElementById('top-menu');
 //2.1
-topMenuEL.style.height-'100%';
+topMenuEl.style.height-'100%';
 //2.2
-topMenuEL.classList.add('flex around');
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+//2.3
+topMenuEl.classList.add('flex-around');
 //3.1 --had issues with this part and need to figure out what I missed
 menuLinks.forEach(function(link) {
     const linkEl = document.createElement('a');
@@ -39,19 +48,19 @@ menuLinks.forEach(function(link) {
     topMenuEl.appendChild(linkEl);
   });
   //4.0
-  const subMenuEl=document.getElementById('sub-menu');
+  const subMenuEl = document.getElementById('sub-menu');
   //4.1
-  subMenuEl.style.height=100%
+  subMenuEl.style.height = '100%';
 //4.2 -- keep getting error ";" expected even after copying solution.
 subMenuEl.style.backgroundColor ; 'var(--sub-menu-bg)';
 //4.3
 subMenuEl.classList.add('flex-around');
 //4.4 
-subMenuEl.style.position='absolute';
+subMenuEl.style.position = 'absolute';
 //4.5
-subMenuEl.style.top='0';
+subMenuEl.style.top = '0';
 //5.1
-const topMenuLinks=document.querySelectorAll('#top-menu a');
+const topMenuLinks = document.querySelectorAll('#top-menu a');
 let showingSubMenu= false;
 //5.2
 topMenuEl.addEventListener('click', function(evt) {
@@ -59,6 +68,7 @@ topMenuEl.addEventListener('click', function(evt) {
     const link = evt.target;
     if (link.tagName !== 'A') return;
     console.log(link.innerText);
+
 //5.3
 if (link.classList.contains('active')) {
     link.classList.remove('active');
@@ -66,7 +76,7 @@ if (link.classList.contains('active')) {
     subMenuEl.style.top = '0';
     return;
   }
-});
+
 //5.4
 topMenuLinks.forEach(function(link) {
     link.classList.remove('active');
@@ -114,7 +124,7 @@ const linkData = menuLinks.find(function(linkObj) {
     subMenuEl.style.top = '0';
     mainEl.innerHTML = '<h1>about</h1>';
   }
-
+});
 
 // Task 5.8
 function buildSubMenu(subLinks) {
@@ -143,9 +153,3 @@ subMenuEl.addEventListener('click', function(evt) {
   // Task 6.3
   mainEl.innerHTML = `<h1>${link.innerText}</h1>`;
 });
-
-
-
-
-
-
